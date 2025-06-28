60c855d3879165a6f5705a67f5812f09098ad265
mwlt_sanodia <mwlt@163.com>
2025-06-28 15:26:54 +0800

    2.6.1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
  * @FilePath: \liuyao_desktop_tauri\README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-# 六爻桌面版
+# 六爻排盘与研究  桌面版
 
 基于 Tauri + Vue3 + TypeScript 开发的六爻桌面应用。
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
  * @Author: mwlt_sanodia mwlt@163.com
  * @Date: 2025-06-25 19:20:01
  * @LastEditors: mwlt_sanodia mwlt@163.com
- * @LastEditTime: 2025-06-28 01:30:17
+ * @LastEditTime: 2025-06-28 04:03:14
  * @FilePath: \liuyao_desktop_tauri\src\components\NavBar.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
